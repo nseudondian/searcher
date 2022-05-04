027e84413aaa5e6e99a83ea0b091a32d5964b930
Nseabasi Udondian <nseabasiudondian@gmail.com>
2022-05-04 23:24:30 +0100

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import './App.css';
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
+
+
+function App() {
+
+
+  const contacts =[
+    {
+      id: "1",
+      "name" : "Nse",
+      "email" : "nse@mail.com"
+    },
+    {
+      id: "2",
+      "name" : "Eno",
+      "email" : "eno@mail.com"
+    }
+  ]
+  return (
+    <div className="ui container">
+      <Header />
+      <AddContact />
+      <ContactList contacts = { contacts} />
+    </div>
+  );
+}
+
+export default App;
